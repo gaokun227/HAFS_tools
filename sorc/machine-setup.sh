13277d91618b1378160b526ed7db6ca19432681c
Kun Gao <Kun.Gao@gaea14.ncrc.gov>
2023-01-06 14:52:51 -0500

    update machine-setup.sh for gaea

--- sorc/machine-setup.sh
+++ sorc/machine-setup.sh
@@ -105,11 +105,36 @@ elif [[ -d /lustre && -d /ncrc ]] ; then
         # before loading the module command.  Without those variables,
         # the module command fails.  Hence we actually have to source
         # /etc/profile here.
-        echo load the module command 1>&2
         source /etc/profile
+        __ms_source_etc_profile=yes
+    else
+        __ms_source_etc_profile=no
+    fi
+    module purge > /dev/null 2>&1
+    module purge
+    # clean up after purge
+    unset _LMFILES_
+    unset _LMFILES_000
+    unset _LMFILES_001
+    unset LOADEDMODULES
+    module load modules
+    if [[ -d /opt/cray/ari/modulefiles ]] ; then
+        module use -a /opt/cray/ari/modulefiles
+    fi
+    if [[ -d /opt/cray/pe/ari/modulefiles ]] ; then
+        module use -a /opt/cray/pe/ari/modulefiles
+    fi
+    if [[ -d /opt/cray/pe/craype/default/modulefiles ]] ; then
+        module use -a /opt/cray/pe/craype/default/modulefiles
+    fi
+    if [[ -s /etc/opt/cray/pe/admin-pe/site-config ]] ; then
+        source /etc/opt/cray/pe/admin-pe/site-config
+    fi
+    if [[ "$__ms_source_etc_profile" == yes ]] ; then
+      source /etc/profile
+      unset __ms_source_etc_profile
     fi
     target=gaea
-    module purge
 elif [[ "$(hostname)" =~ "odin" ]]; then
     target="odin"
 else
